1360db0d37a1f5423d390ffab59fbca1c8ce79d3
JohnTitor <32987636+JohnElricTitor@users.noreply.github.com>
2020-11-06 00:25:02 -0500

    Online
    Project so far has a grid system that spawns grid according to requested size with the amount of requested walking tiles accounted for. Still lacks scriptable object implementation and start and end points. (bit of the lighting settings where changed but desired lighting is still not implemented)

---
+++ SlidingPuzzle/Assets/Scripts/GridManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private int[,] tiles;                   //2D array storing all the types of tiles
+    [SerializeField] GameObject[] tileTypes = null;
+    [SerializeField] int gridX = 0;
+    [SerializeField] int gridZ = 0;
+    [SerializeField] float gridSpacingOffset = 2f;
+    public Vector3 gridOrigin = Vector3.zero;
+
+    [SerializeField] int pathLength = 0;
+    private int pathCount = 0;
+    bool isDeadSpace = false;
+
+    [SerializeField] GameObject empty;
+
+    private void Start()
+    {
+        tiles = new int[gridX,gridZ];
+        GenerateGrid();
+        SpawnGrid();
+        EmptySpace();
+    }
+
+    void GenerateGrid() 
+    {
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int z = 0; z < gridZ; z++)
+            {
+                tiles[x, z] = Random.Range(1, tileTypes.Length);
+            }
+        }
+        CreatePath();
+    }
+
+    void CreatePath()
+    {
+        while (pathCount < pathLength)          //while the amount of pathTiles spawned is less than the pathLength
+        {
+            int randX = Random.Range(0, gridX); //Get a random number
+            int randZ = Random.Range(0, gridZ); //Get a random number
+
+            if (tiles[randX,randZ] != 0)        //Check a tile and if it's not of type pathTile
+            {
+                tiles[randX, randZ] = 0;
+                pathCount++;
+            }
+        }
+    }
+
+    void SpawnGrid()
+    {
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int z = 0; z < gridZ; z++)
+            {
+                Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, 0, z * gridSpacingOffset) + gridOrigin;
+                GameObject clone = Instantiate(tileTypes[tiles[x,z]], spawnPosition, Quaternion.identity);
+
+                clone.transform.parent = this.transform;
+            }
+        }
+    }
+    
+
+    void EmptySpace()
+    {
+        while(!isDeadSpace)
+        {
+            int rand = Random.Range(0, transform.childCount);
+            if (transform.GetChild(rand).GetComponent<TileType>().Type != 0)
+            {
+                GameObject blank = Instantiate(empty, transform.GetChild(rand).position, Quaternion.identity);
+                blank.transform.parent = this.transform;
+                Destroy(transform.GetChild(rand).gameObject);
+                isDeadSpace = true;
+            }
+
+        }
+    }
+}
---
+++ SlidingPuzzle/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    GameObject empty;
+    float xTemp;
+    float zTemp;
+
+    [SerializeField] float rayLength;
+
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            GameObject empty = GameObject.Find("Empty(Clone)");
+            if (Physics.Raycast(ray, out hit, rayLength))
+            {
+               if(Vector3.Distance(hit.transform.position, empty.transform.position) == 2)
+               {
+                    xTemp = hit.transform.position.x;
+                    zTemp = hit.transform.position.z;
+                    hit.transform.position = new Vector3(empty.transform.position.x, 0, empty.transform.position.z);
+                    empty.transform.position = new Vector3(xTemp, 0, zTemp);
+               }
+            }
+        }
+    }
+}
