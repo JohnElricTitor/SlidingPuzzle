f9ebbae4460df7bcd87c34c2b45f67484f76031a
JohnTitor <32987636+JohnElricTitor@users.noreply.github.com>
2021-04-26 01:34:45 -0400

    Fixed Lighting and orientation
    moved project to new unity folder and updated version of unity. Emission fixed on some phones. Reimported all tiles with fixed rotation and added new levels and tiles for levels

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//THIS SCRIPT SIMPLY CONTROLS THE CAMERA ALLOWING YOU TO ROTATE IT AROUND THE CAMERA GIMBAL 
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 prevPos;                                                                        //STORE THE PREVIOUS POSITION OF THE CAMERA
+
+    private void Start()
+    {
+        enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            prevPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        
+        if (Input.GetMouseButton(0) && Input.mousePosition.y < Screen.height / 4)
+            CameraTurn();
+    }
+
+    void CameraTurn()
+    { 
+        Vector3 direction = prevPos - Camera.main.ScreenToViewportPoint(Input.mousePosition);  //DIRECTION = THE CAMERAS CURRENT POSITION - DIRECTION OF MOUSE 
+        transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);               //ROTATE CAMERA ON Y AIX BY NEGATIVE X DIRECTION IN WORLD SPACE
+        prevPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);                      //UPDATE THE prevPos THE CAMERAS POSITION;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EventManager))]
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject title = null;
+    [SerializeField] Vector3 pausePos = new Vector3 (0,0,0);
+    [SerializeField] Vector3 playPos = new Vector3(0, 0, 0);
+    [SerializeField] bool isPaused = true;
+    private Animator anim = null;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnEnable()
+    {
+        EventManager.OnUI += MenuState;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnUI -= MenuState;
+    }
+    private void Update()
+    {
+        if (isPaused)
+            Pause();
+        if (!isPaused)
+            Play();
+    }
+
+    void MenuState()
+    {
+        isPaused = !isPaused;
+        anim.SetBool("isPaused", isPaused);
+    }
+    void Pause()
+    {
+        if(title.activeSelf != true)
+            title.SetActive(true);
+        if(title.transform.position != pausePos)
+            title.transform.position = Vector3.MoveTowards(title.transform.position, pausePos, 0.2f);
+    }
+    
+    void Play()
+    {
+        if(title.transform.position != playPos)
+            title.transform.position = Vector3.MoveTowards(title.transform.position, playPos, 0.2f);
+        if (title.transform.position == playPos)
+            title.SetActive(false);
+    }
+
+
+}
+
